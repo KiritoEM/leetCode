1c36aa80869054aba6a8d3b21a35911149debd7b
KiritoEM <146085343+KiritoEM@users.noreply.github.com>
2024-08-03 09:28:38 +0300

    Time: 92 ms (27.30%) | Memory: 54.9 MB (5.28%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    if (x < 2) return x;
+    let start = 1;
+    let end = x;
+    let mid;
+
+    while (start <= end) {
+        mid = Math.floor(start + (end - start) / 2);
+
+        if (BigInt(mid) * BigInt(mid) === BigInt(x)) {
+            return mid;
+        } else if (BigInt(mid) * BigInt(mid) > BigInt(x)) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    return end;
+};
