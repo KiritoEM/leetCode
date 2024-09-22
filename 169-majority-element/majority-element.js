32c0ac25dd271dec06b895eb3641f5c726af52bd
KiritoEM <146085343+KiritoEM@users.noreply.github.com>
2024-09-22 12:57:25 +0300

    Time: 53 ms (87.52%) | Memory: 52.6 MB (40.47%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let candidate = null;
+    let count = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+
+    return candidate;
+};
