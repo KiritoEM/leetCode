7cb0f01ee7c6927cbfe0430696227c805b681f35
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2024-09-28 19:38:29 +0300

    Time: 67 ms (88.59%) | Memory: 53.3 MB (91.81%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let j = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[j++] = nums[i]
+        }
+    }
+
+    while (j < nums.length) {
+        nums[j++] = 0;
+    }
+};
