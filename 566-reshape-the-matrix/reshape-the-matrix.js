c34ccdbd37e33b764fd12d6145ba22402061453f
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2024-10-03 14:49:04 +0200

    Time: 72 ms (78.25%) | Memory: 54.1 MB (68.42%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.js
+++ 566-reshape-the-matrix/reshape-the-matrix.js
@@ -7,17 +7,16 @@
 var matrixReshape = function (mat, r, c) {
     let m = mat.length;
     let n = mat[0].length;
-    let flat_matrix = mat.flat()
-    let flatten = [[]]
 
     if (m * n !== r * c) return mat;
 
-    for (let i = 0; i < r; i++) {
-        flatten[i] = []
-        for (let j = 0; j < c; j++) {
-            flatten[i][j] = flat_matrix[c * i + j];
-        }
+    let flat_matrix = mat.flat();
+    let flatten = Array.from({ length: r }, () => new Array(c));
+
+    for (let i = 0; i < r * c; i++) {
+        flatten[Math.floor(i / c)][i % c] = flat_matrix[i];
     }
 
     return flatten;
+
 }
