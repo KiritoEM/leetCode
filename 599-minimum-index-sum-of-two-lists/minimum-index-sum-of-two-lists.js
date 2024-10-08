3fbb321929a2ca0e4a5bb10e6b01b04377d6268d
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2024-10-08 09:05:18 +0200

    Time: 80 ms (71.92%) | Memory: 58.9 MB (27.32%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.js
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.js
@@ -7,20 +7,22 @@ var findRestaurant = function (list1, list2) {
     let dict = {};
     let result = [];
 
+    // Créer un dictionnaire pour stocker les indices des éléments de list1
     for (let i = 0; i < list1.length; i++) {
-        for (let j = 0; j < list2.length; j++) {
-            if (list1[i] === list2[j]) {
-                dict[list1[i]] = i + j;
-            }
-        }
+        dict[list1[i]] = i;
     }
 
-    let Keys = Object.values(dict);
-    minIndex = Math.min(...Keys);
-
-    for (const [k, v] of Object.entries(dict)) {
-        if (v === minIndex) {
-            result.push(k);
+    // Parcourir list2 et vérifier si chaque élément existe dans le dictionnaire de list1
+    let minIndex = Infinity;
+    for (let j = 0; j < list2.length; j++) {
+        if (dict.hasOwnProperty(list2[j])) {
+            let indexSum = dict[list2[j]] + j;
+            if (indexSum < minIndex) {
+                minIndex = indexSum;
+                result = [list2[j]]; // Réinitialiser la liste des résultats
+            } else if (indexSum === minIndex) {
+                result.push(list2[j]); // Ajouter à la liste des résultats
+            }
         }
     }
 
