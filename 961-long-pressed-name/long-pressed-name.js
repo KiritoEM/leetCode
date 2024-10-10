29d3fe1148031291151012aba0bcb7dc1cfc032e
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2024-10-10 09:04:39 +0200

    Time: 116 ms (5.24%) | Memory: 58 MB (5.24%) - LeetSync

---
+++ 961-long-pressed-name/long-pressed-name.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function (name, typed) {
+    let n_p = 0;
+    let t_p = 0;
+
+    while (t_p < typed.length) {
+        console.log(n_p, name.length)
+        if (name[n_p] === typed[t_p]) {
+            n_p++;
+            t_p++;
+        }
+        else if (t_p > 0 && typed[t_p] === typed[t_p - 1]) {
+            t_p++;
+        }
+        else {
+            return false;
+        }
+
+    }
+    return n_p === name.length;
+};
