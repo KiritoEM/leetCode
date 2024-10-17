b48ac0df8ec9c132de6c20293527e81785e59d8e
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2024-10-17 03:15:26 +0200

    Time: 68 ms (5.10%) | Memory: 49.2 MB (20.28%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -5,11 +5,12 @@
  */
 var removeElement = function (nums, val) {
     let k = 0;
+
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== val) {
-            nums[k] = nums[i];
-            k++;
+            nums[k++] = nums[i];
         }
     }
+
     return k;
-}
+};
