8b963ea60dc17ebf58d4a533b8921060435b99b7
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2024-10-20 21:27:31 +0200

    Time: 1 ms (99.84%) | Memory: 53 MB (73.66%) - LeetSync

---
+++ 55-jump-game/jump-game.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+    let n = nums.length;
+    let maxReach = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (i > maxReach) return false;
+        maxReach = Math.max(maxReach, i + nums[i]);
+    }
+
+    return true;
+};
