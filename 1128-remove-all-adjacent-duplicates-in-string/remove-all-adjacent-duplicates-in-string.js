eb5952c56ff1fcdeaaf5881ddc72e8acbd15f236
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2024-10-26 09:37:53 +0300

    Time: 14 ms (92.90%) | Memory: 57.4 MB (76.43%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function (s) {
+    let stack = [];
+
+    for (let char of s) {
+        if (stack[stack.length - 1] === char) {
+            stack.pop();
+        }
+        else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join("");
+};
