5c2a0b23c8edb0e539a429b9ff9616c527818d39
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2024-10-27 21:38:25 +0300

    Time: 0 ms (100.00%) | Memory: 49.2 MB (38.40%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function (std, sdw) {
+    let unable = 0;
+
+    while (std.length) {
+        if (std[0] === sdw[0]) {
+            std.shift();
+            sdw.shift();
+            unable = 0;
+        }
+        else {
+            std.push(std.shift()); //revoie a la fin de la queue
+            unable++;
+            if (std.length === unable) break;
+        }
+    }
+
+    return std.length;
+};
