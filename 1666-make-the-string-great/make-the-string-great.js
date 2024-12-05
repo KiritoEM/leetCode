91fc43d7fe3e6c69eb9e79ff600a56f776b4d3bb
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2024-12-05 09:36:44 +0300

    Time: 5 ms (28.97%) | Memory: 51.1 MB (54.70%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.js
+++ 1666-make-the-string-great/make-the-string-great.js
@@ -7,7 +7,6 @@ var makeGood = function (s) {
 
     for (let i = 0; i < s.length; i++) {
         if ((s[i] === s[i].toLowerCase() && s[i].toUpperCase() === stack.at(-1) || (s[i] === s[i].toUpperCase() && s[i].toLowerCase() === stack.at(-1)))) {
-            console.log("stack: ", stack.at(-1), "current letter: ", s[i])
             stack.pop();
         }
         else {
