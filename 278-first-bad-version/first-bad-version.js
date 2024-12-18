f1f2c64d0a57bbb1635ba928221590d4cf4b5f2a
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2024-12-18 17:22:32 +0300

    Time: 73 ms (5.17%) | Memory: 50.4 MB (6.62%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let start = 1, end = n;
+        let val;
+        let nbr;
+
+        while (true) {
+            nbr = Math.floor((end + start) / 2)
+            val = isBadVersion(nbr);
+            console.log("val: ", nbr)
+            if (val) {
+                if (!isBadVersion(nbr - 1))
+                    return nbr;
+                end = nbr - 1;
+            }
+            else {
+                if (isBadVersion(nbr + 1))
+                    return nbr + 1;
+                start = nbr + 1;
+            }
+
+
+        }
+
+    };
+};
